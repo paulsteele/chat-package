4421420e74eb7cb33728e68167301d2a2e03f02d
paulsteele <paul_steele@live.com>
2013-12-15 11:38:33 -0500

    created Server file

---
+++ src/server/Server.java
@@ -0,0 +1,15 @@
+package server;
+/**
+ * 
+ * @author Paul Steele
+ * Author website : paul-steele.com
+ * Project page : https://github.com/paulsteele/chat-package
+ * 
+ * This file is protected under a creative commons license. For further information see paul-steele.com/license.php
+ * 
+ * Server for the chat package. Run via command line.
+ *
+ */
+public class Server {
+
+}

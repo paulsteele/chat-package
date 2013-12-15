3b856d87eb7ca35d6443c0d5add79f86c1063407
paulsteele <paul_steele@live.com>
2013-12-15 11:40:44 -0500

    created client files

---
+++ src/client/View.java
@@ -0,0 +1,15 @@
+package client;
+/**
+ * 
+ * @author Paul Steele
+ * Author website : paul-steele.com
+ * Project page : https://github.com/paulsteele/chat-package
+ * 
+ * This file is protected under a creative commons license. For further information see paul-steele.com/license.php
+ * 
+ * View for the client for the chat package.
+ *
+ */
+public class View {
+
+}

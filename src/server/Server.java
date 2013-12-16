1da6271975f1ad47bf4e69e0e5cabb01849bee6c
paulsteele <paul_steele@live.com>
2013-12-15 19:51:07 -0500

    Constructor created for Server and base code added

--- src/server/Server.java
+++ src/server/Server.java
@@ -1,4 +1,10 @@
 package server;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.LinkedList;
+
 /**
  * 
  * @author Paul Steele
@@ -12,5 +18,24 @@ package server;
  *
  */
 public class Server {
+	private LinkedList<Client> clients;
+	private int port;
+	
+	public Server(int port) {
+		this.port  = port;
+	}
+	
+	public static void main(String args) throws IOException{
+		ServerSocket connection = new ServerSocket(8888);
+		
+		
+	}
+}
 
+class Client {
+	private Socket thisSocket;
+	
+	public Client(Socket s) {
+		thisSocket = s;
+	}
 }
